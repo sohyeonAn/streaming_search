2eee4d425fbad02730dd58cfa117be1fe42983d7
sohyeonAn <ansh4393@gmail.com>
2022-02-25 22:46:24 +0900

    ✨ 무한 스크롤 기능 추가

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -9,31 +9,37 @@ const StyledButton = styled.button`
   position: fixed;
   bottom: 2rem;
   right: 2rem;
+  width: 2rem;
+  height: 2rem;
   font-size: 1.5rem;
   border-radius: 50%;
   box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3);
 
   &:hover {
     color: #fff;
-    background-color: #1b4f72;
+    background-color: #316588;
   }
 `;
 
 function Home() {
   const [showButton, setShowButton] = useState(false);
   const [searchInput, setSearchInput] = useState("");
+
   const onInsert = useCallback((input) => {
     setSearchInput(input);
   }, []);
 
+  const scrollToTopHandler = () => {
+    if (window.scrollY > 300) {
+      setShowButton(true);
+    } else {
+      setShowButton(false);
+    }
+  };
+
   useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 300) {
-        setShowButton(true);
-      } else {
-        setShowButton(false);
-      }
-    });
+    window.addEventListener("scroll", scrollToTopHandler);
+    return () => window.removeEventListener("scroll", scrollToTopHandler);
   }, []);
 
   const scrollToTop = () => {
--- src/components/home/ItemList.js
+++ src/components/home/ItemList.js
@@ -1,9 +1,9 @@
-import usePromise from "../../lib/usePromise";
 import styled from "styled-components";
 import Responsive from "../common/Responsive";
 import { API_PARAMS } from "../../constants/API";
 import axios from "axios";
 import Item from "./Item";
+import React, { useState, useEffect, useRef } from "react";
 
 const ItemListBlock = styled(Responsive)`
   padding-top: 1.5rem;
@@ -12,53 +12,103 @@ const ItemListBlock = styled(Responsive)`
   gap: 1rem;
   row-gap: 2rem;
 `;
+
 function ItemList({ searchInput }) {
   const params = {
     ...API_PARAMS,
     query: searchInput,
   };
+  const currPage = useRef(1);
+  const scrollRemember = useRef(0);
+  const [result, setResult] = useState([]);
+  const [error, setError] = useState(null);
+  const [hasMore, setHasMore] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const [loading, response, error] = usePromise(() => {
-    return axios.get("https://api.themoviedb.org/3/search/multi?", {
-      params,
+  const scrollBottomHandler = () => {
+    if (error || isLoading || !hasMore) return;
+    const { innerHeight, scrollY } = window;
+    const { scrollHeight } = document.body;
+
+    if (scrollY >= scrollHeight - innerHeight) {
+      scrollRemember.current = scrollY;
+      setTimeout(loadData, 300);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollBottomHandler);
+    window.scrollTo({
+      top: scrollRemember.current,
     });
+    return () => window.removeEventListener("scroll", scrollBottomHandler);
+  });
+
+  useEffect(() => {
+    setResult([]);
+    currPage.current = 1;
+    scrollRemember.current = 0;
+
+    loadData();
   }, [searchInput]);
 
-  if (loading) {
-    return <ItemListBlock>불러오는 중...</ItemListBlock>;
-  }
+  const loadData = async () => {
+    console.log("loadData() called");
 
-  if (!response) {
-    return null;
-  }
+    setIsLoading(true);
+    try {
+      const response = await axios.get(
+        "https://api.themoviedb.org/3/search/multi?",
+        { params: { ...params, page: currPage.current } }
+      );
+      if (!response) {
+        return;
+      }
+      if (currPage.current < response.data.total_pages) {
+        setHasMore(true);
+        currPage.current += 1;
+      } else if (currPage.current === response.data.total_pages) {
+        setHasMore(false);
+        window.removeEventListener("scroll", scrollBottomHandler);
+      }
+      setIsLoading(false);
+
+      const { results } = response.data;
+      setResult((prevResult) => [...prevResult, ...results]);
+    } catch (e) {
+      setError(e);
+      setIsLoading(false);
+    }
+  };
 
   if (error) {
-    return <ItemListBlock>에러 발생!</ItemListBlock>;
+    return <div>에러 발생! {error.message}</div>;
+  }
+  if (isLoading) {
+    return <div>불러 오는 중...</div>;
   }
 
-  const { results } = response.data;
   return (
     <>
       <ItemListBlock>
-        {results &&
-          results.map((item) => {
-            if (item.media_type === "person") {
-              return null;
-            } else {
-              return (
-                <Item
-                  key={item.id}
-                  id={item.id}
-                  title={item.media_type === "tv" ? item.name : item.title}
-                  thumbnail={item.poster_path}
-                  media_type={item.media_type}
-                />
-              );
-            }
-          })}
+        {result.map((item) => {
+          if (item.media_type === "person") {
+            return null;
+          }
+          return (
+            <Item
+              key={`${item.media_type}-${item.id}`}
+              id={item.id}
+              title={item.media_type === "tv" ? item.name : item.title}
+              thumbnail={item.poster_path}
+              media_type={item.media_type}
+            />
+          );
+        })}
       </ItemListBlock>
+      {!hasMore && <div>모든 정보를 읽어왔습니다.</div>}
     </>
   );
 }
 
-export default ItemList;
+export default React.memo(ItemList);
