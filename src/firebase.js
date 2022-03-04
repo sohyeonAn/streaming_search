526d0a20f56e43a47b5ce97c0af7c4db8ff2f6a1
sohyeonAn <ansh4393@gmail.com>
2022-03-04 16:43:41 +0900

    ➕ firebase 추가

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDXwjjWSJJhRX72_wHfC4O5sgMckyvEYIU",
+  authDomain: "streaming-search-9f63c.firebaseapp.com",
+  projectId: "streaming-search-9f63c",
+  storageBucket: "streaming-search-9f63c.appspot.com",
+  messagingSenderId: "316368840027",
+  appId: "1:316368840027:web:d8c9c413a98f9cf0df667e",
+  measurementId: "G-RXEWQJDKB0",
+};
+
+const app = initializeApp(firebaseConfig);
