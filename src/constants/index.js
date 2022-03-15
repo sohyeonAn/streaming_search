a9a3cad4c06965cb815f2cdc53fd0ce37190752d
sohyeonAn <ansh4393@gmail.com>
2022-03-15 12:30:23 +0900

    🚚 검색 조건 constants에 추가

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,3 +4,7 @@ export const API_PARAMS = {
 };
 
 export const IMAGE_PATH = "https://image.tmdb.org/t/p/original";
+
+export const MEDIA_TYPE = ["movie", "tv"];
+export const PROVIDER = ["넷플릭스", "왓챠", "네이버스토어", "웨이브"];
+export const PROVIDE_TYPE = ["구매", "렌트", "스트리밍"];
--- src/containers/basket/BasketContainer.js
+++ src/containers/basket/BasketContainer.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Filter from "../../components/common/Filter";
 import Responsive from "../../components/common/Responsive";
-
+import { MEDIA_TYPE, PROVIDER, PROVIDE_TYPE } from "../../constants/index";
 const BasketBlock = styled.div`
   .title {
     padding: 1rem;
@@ -15,9 +15,6 @@ const FilterBlock = styled.div`
   justify-content: center;
   gap: 2rem;
 `;
-const mediaType = ["movie", "tv"];
-const provider = ["넷플릭스", "왓챠", "네이버스토어", "웨이브"];
-const provideType = ["구매", "렌트", "스트리밍"];
 
 function BasketContainer() {
   return (
@@ -25,9 +22,9 @@ function BasketContainer() {
       <BasketBlock>
         <h2 className="title">✨ 나의 찜 리스트 ✨</h2>
         <FilterBlock>
-          <Filter selectTitle="미디어타입" selectList={mediaType} />
-          <Filter selectTitle="제공사" selectList={provider} />
-          <Filter selectTitle="제공형태" selectList={provideType} />
+          <Filter selectTitle="미디어타입" selectList={MEDIA_TYPE} />
+          <Filter selectTitle="제공사" selectList={PROVIDER} />
+          <Filter selectTitle="제공형태" selectList={PROVIDE_TYPE} />
         </FilterBlock>
       </BasketBlock>
     </Responsive>
