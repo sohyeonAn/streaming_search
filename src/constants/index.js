2b4c5a15f0d420a89bddfe994ee1da45b918d436
sohyeonAn <ansh4393@gmail.com>
2022-03-15 14:29:24 +0900

    💬 한영 매핑단어 추가

--- src/constants/index.js
+++ src/constants/index.js
@@ -10,6 +10,13 @@ export const WORDS_MAPPING = {
   제공사: "provider",
   제공형태: "provideType",
   전체: "all",
+  구매: "buy",
+  렌트: "rent",
+  스트리밍: "flatrate",
+  "네이버 시리즈": "Naver Store",
+  넷플릭스: "Netflix",
+  왓챠: "watcha",
+  웨이브: "wavve",
 };
 export const MEDIA_TYPE = ["movie", "tv"];
 export const PROVIDER = ["넷플릭스", "왓챠", "네이버스토어", "웨이브"];
