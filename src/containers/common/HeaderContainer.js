cb6a43b7c494882ea99f7328a9566ab553fe13f5
sohyeonAn <ansh4393@gmail.com>
2022-03-16 15:36:48 +0900

    🎨 로그아웃 시 메인화면으로 이동

--- src/containers/common/HeaderContainer.js
+++ src/containers/common/HeaderContainer.js
@@ -2,13 +2,16 @@ import Header from "../../components/common/Header";
 import { useStateValue } from "../../contexts/StateProvider";
 import { auth } from "../../firebase";
 import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 function HeaderContainer() {
+  const navigator = useNavigate();
   const [{ user }, dispatch] = useStateValue();
 
   const onLogout = () => {
     if (user) {
       signOut(auth);
+      navigator("/");
     }
   };
   return <Header user={user} onLogout={onLogout} />;
